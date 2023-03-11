d1abd73e2c77c833c323b88b7e5245a68fd7dcd4
waleed <waleed.ayoub@gmail.com>
2023-03-11 18:57:06 +0000

    started week 4

---
+++ backend-flask/db/seed.sql
@@ -0,0 +1,29 @@
+-- manually created data
+
+INSERT INTO
+    public.users (
+        display_name,
+        handle,
+        cognito_user_id
+    )
+VALUES (
+        'Cruddur Dude',
+        'cruddurman',
+        'MOCK'
+    ), (
+        'Andrew Bayko',
+        'bayko',
+        'MOCK'
+    );
+
+INSERT INTO
+    public.activities (user_uuid, message, expires_at)
+VALUES ( (
+            SELECT uuid
+            from public.users
+            WHERE
+                users.handle = 'andrewbrown'
+            LIMIT
+                1
+        ), 'This was imported as seed data!', current_timestamp + interval '10 day'
+    )
--- journal/week4.md
+++ journal/week4.md
@@ -1,3 +1,9 @@
 # Week 4 — Postgres and RDS
 
 ## Lecture Notes
+- AWS Aurora vs AWS RDS Postgrs, which is better?
+    - Depends on use case, RDS Posgres is way cheaper and I assume less fully featured?
+
+- Used the aws-cli to deploy an RDS Postgres instance
+- Important to note that RDS Postgres can only be stopped temporarily for 7 days!!!
+    - After that, it turns back on and starts to incur spend
