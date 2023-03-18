2932ae8d491025d8c22fd310f2408dc675b3d6f9
waleed <waleed.ayoub@gmail.com>
2023-03-18 17:42:11 +0000

    caught up to week 4 - errors in sql

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -115,13 +115,12 @@ def data_home():
     app.logger.debug(claims)
     app.logger.debug(claims['username'])
     data = HomeActivities.run(cognito_user_id=claims['username'])
-    return data, 200
   except TokenVerifyError as e:
     # unauthenicatied request
     app.logger.debug(e)
     app.logger.debug("unauthenicated")
     data = HomeActivities.run()
-    return data, 200
+  return data, 200
 
 
 @app.route("/api/activities/notifications", methods=["GET"])
--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,29 +1,13 @@
--- manually created data
+-- this file was manually created
+INSERT INTO public.users (display_name, handle, cognito_user_id)
+VALUES
+  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
+  ('Andrew Bayko', 'bayko' ,'MOCK');
 
-INSERT INTO
-    public.users (
-        display_name,
-        handle,
-        cognito_user_id
-    )
-VALUES (
-        'Cruddur Dude',
-        'cruddurman',
-        'MOCK'
-    ), (
-        'Andrew Bayko',
-        'bayko',
-        'MOCK'
-    );
-
-INSERT INTO
-    public.activities (user_uuid, message, expires_at)
-VALUES ( (
-            SELECT uuid
-            from public.users
-            WHERE
-                users.handle = 'andrewbrown'
-            LIMIT
-                1
-        ), 'This was imported as seed data!', current_timestamp + interval '10 day'
-    )
+INSERT INTO public.activities (user_uuid, message, expires_at)
+VALUES
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    'This was imported as seed data!',
+    current_timestamp + interval '10 day'
+  )
